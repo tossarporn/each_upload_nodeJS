8802997424991ed084db85b26d085f05b06d18e9
tossarporn supanikorn <tossarporn@gmail.com>
2025-06-18 15:16:55 +0700

    upload file

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 //server.js
 const express = require('express');
+const multer = require('multer');
+//dest:'uploads/' คืออัพโหลดไปยังไฟล์ uploads/
+const upload = multer({dest:'uploads/'});
 const cors = require('cors');
 
 const port = '8000';
@@ -7,6 +10,14 @@ const port = '8000';
 const app = express();
 app.use(cors());
 
+//app.post('/part',Middleware=>upload.single('key'),(req,res)
+app.post('/upload',upload.single('test'),(req,res)=>{
+    //1.ตัวแปรที่จะรับชื่อตัวแปร อะไร
+    //2.part ที่จะเอาไฟล์ไปวาง
+    res.json({
+        message:"Hello upload"
+    });
+})
 
 app.listen(port,()=>{
     console.log(`Server started on http://localhost:${port}`)
